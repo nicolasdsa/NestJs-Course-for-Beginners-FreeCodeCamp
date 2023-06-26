46ed1e7c1972ce03c3a1786f5b2a26917b894637
Nicolas Sosa Machado <nicolassosamachado25@gmail.com>
2023-06-26 01:05:34 -0300

    corrigindo bug

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { ConfigService } from '@nestjs/config';
+
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor(config: ConfigService) {
