478f4d3e7a8981450ef1b04588c595a729a563b8
nicolas <nicolassosamachado25@gmail.com>
2023-06-29 23:49:56 -0300

    custom param decorators e e2e tests

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
-import { JwtGuard } from 'src/auth/guard';
+import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { getUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
 
 @Controller('users')
 export class UserController {
   //Pode ser passado direto   @UseGuards(AuthGuard('jwt')), mas criar um custom guard evita erros.
+  //O parametro pode ser passado assim:@Req() req: Request e acessar req.user
+  //porém podemos criar um custom decorator para facilitar
+  //a leitura
   @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@getUser() user: User, @getUser('email') email: string) {
+    console.log(email);
+    return user;
   }
+
+  @Patch('me')
+  editUser() {}
 }
